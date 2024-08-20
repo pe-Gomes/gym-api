3ed8b7bdbc19a8c526b104b555d2c7f93e6712d5
Pedro Gomes <pcgbernardes@gmail.com>
2024-08-20 16:10:10 -0400

    fix(bug): resolve date reference in memory issues

--- src/repositories/prisma-check-ins-repository.ts
+++ src/repositories/prisma-check-ins-repository.ts
@@ -1,5 +1,5 @@
 import { db } from '@/infra/db'
-import { Prisma } from '@prisma/client'
+import { CheckIn, Prisma } from '@prisma/client'
 import { CheckInsRepository } from './check-ins-repository'
 
 export class PrismaCheckInsRepository implements CheckInsRepository {
@@ -8,8 +8,8 @@ export class PrismaCheckInsRepository implements CheckInsRepository {
   }
 
   async findByUserIdOnDate(userId: string, date: Date) {
-    const from = date
-    const to = date
+    const from = new Date(date)
+    const to = new Date(date)
     from.setHours(0, 0, 0, 0)
     to.setHours(23, 59, 59, 999)
 
@@ -20,6 +20,27 @@ export class PrismaCheckInsRepository implements CheckInsRepository {
           gte: from,
           lte: to,
         },
+      },
+    })
+  }
+
+  async findManyByUserIdAndPaginate({
+    userId,
+    page,
+    limit,
+  }: {
+    userId: string
+    page?: number
+    limit?: number
+  }): Promise<CheckIn[]> {
+    return await db.checkIn.findMany({
+      where: {
+        userId,
+      },
+      take: limit ?? undefined,
+      skip: page ? (page - 1) * (limit ?? 0) : undefined,
+      orderBy: {
+        createdAt: 'desc',
       },
     })
   }
