4c02e5f8fe1dd3e1ef547b22990d2d6032afe37d
Pedro Gomes <pcgbernardes@gmail.com>
2024-09-06 09:43:19 -0400

    feat(route):update app routes

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -2,20 +2,12 @@ import { FastifyInstance } from 'fastify'
 
 import { verifyJWT } from '@/http/middlewares/verify-jwt'
 
-import { handleGetUserMetrics } from '../get-user-metrics'
 import { handleAuthentication } from './authentication'
 import { handleGetUserProfile } from './get-user-profile'
 import { handleRegister } from './register'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', handleRegister)
-  app.get(
-    '/users/:userId/check-in/metrics',
-    {
-      onRequest: [verifyJWT],
-    },
-    handleGetUserMetrics
-  )
 
   app.post('/sessions', handleAuthentication)
 
--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,4 +1,5 @@
 import { gymsRoutes } from './controllers/gyms/routes'
 import { usersRoutes } from './controllers/users/routes'
+import { checkInsRoutes } from './controllers/check-ins/routes'
 
-export { gymsRoutes, usersRoutes }
+export { gymsRoutes, usersRoutes, checkInsRoutes }
